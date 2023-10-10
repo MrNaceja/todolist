2735d78a104c0f5b9fca063392368bca4b853dda
Eduardo Toriani <edutoriani13@gmail.com>
2023-10-10 09:38:39 -0300

    Aula 2 finalizada.;

--- src/main/java/br/com/eduardotoriani/todolist/user/UserController.java
+++ src/main/java/br/com/eduardotoriani/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.eduardotoriani.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
+
+    @Autowired
+    private IUserRepository repository;
+  
     @PostMapping("/")
-    public void create(@RequestBody UserModel user) {
-        System.out.println("Usuário: " + user.username);
+    public ResponseEntity create(@RequestBody UserModel user) {
+        var userFounded = repository.findByUsername(user.getUsername());
+        if (userFounded != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ops, já existe um usuário com este nome!");
+        }
+        var userCreated = repository.save(user);
+        return ResponseEntity.ok().body(userCreated);
     }
 
 }
